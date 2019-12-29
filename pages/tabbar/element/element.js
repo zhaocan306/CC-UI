0a0b6a84c8924a69e87d524e866db946ecc30321
can.zhao <can.zhao@aliyun.com>
2019-12-29 16:29:02 +0800

    index
    index

---
+++ pages/tabbar/element/element.js
@@ -0,0 +1,59 @@
+Page({
+  data: {
+    dataList: [
+      {
+        name: '布局',
+        icon: 'icon-ai13',
+        tip: 'layout'
+      },
+      {
+        name: '文本',
+        icon: 'icon-wenben',
+        tip: 'text'
+      },
+      {
+        name: '色彩',
+        icon: 'icon-theme',
+        tip: 'color'
+      },
+      {
+        name: '阴影',
+        icon: 'icon-box-shadow',
+        tip: 'shadow'
+      },
+      {
+        name: '图标',
+        icon: 'icon-info',
+        tip: 'icon'
+      },
+      {
+        name: '头像',
+        icon: 'icon-touxiang',
+        tip: 'identity'
+      },
+      {
+        name: '按钮',
+        icon: 'icon-anniuzu',
+        tip: 'botton'
+      },
+      {
+        name: '标签',
+        icon: 'icon-tag',
+        tip: 'tag'
+      },
+      {
+        name: '加载',
+        icon: 'icon-jiazai',
+        tip: 'loading'
+      },
+      {
+        name: '进度条',
+        icon: 'icon-jindutiao',
+        tip: 'progress'
+      }
+    ]
+  },
+  onLoad(options) {
+
+  }
+})
